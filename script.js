bb5448270def6b754cbc01848f15b65cf8cc324d
lacnoskillz <kaikane3457@hotmail.com>
2022-11-09 13:10:38 -0600

    attempting to grab user input from html to java and failing

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
+var buttonEL = $("button");
+
+var hour9EL = $('#hour-9["textarea"]');
+
+
+buttonEL.on("click", function () {
+  var input = hour9EL.val();
+  console.log(input);
+  localStorage.setItem("new input",input);
+
+
+
+});
+ 
+
+
+
+
+
+
+
+// TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -20,4 +40,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
